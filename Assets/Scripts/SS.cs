1746f883bafacdbcb8a88f7d41e0912cfe07a095
Aditya Vartak <aadivartak@gmail.com>
2019-02-11 16:18:16 +0530

    11/2/19

---
+++ Assets/Scripts/SS.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SS : MonoBehaviour
+{
+     public int resWidth = 640; 
+     public int resHeight = 480;
+ 	 public int fps=15;
+ 	 private float videoTime=0;
+     private bool takeHiResShot = false;
+     private bool robotlocupdate = false;
+ 
+     public static string ScreenShotName(int width, int height) {
+         return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
+                              Application.dataPath, 
+                              width, height, 
+                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss.ffff"));
+     }
+ 
+     public void TakeHiResShot() {
+         takeHiResShot = true;
+     }
+     void Update(){
+            videoTime += Time.deltaTime;
+            Debug.LogFormat(((float)fps).ToString());
+ 
+            if (videoTime >= (1f / (float)fps)) {
+            	//Debug.LogFormat(videoTime.ToString());
+                videoTime = 0;
+                takeHiResShot=true;
+                robotlocupdate=true;
+
+                if(robotlocupdate){
+                	Vector3 pos=GameObject.Find("FirstPersonCharacter").transform.position;
+                	Debug.Log(pos);
+                }
+
+                 if (takeHiResShot) {
+             RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+             GetComponent<Camera>().targetTexture = rt;
+             Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+             GetComponent<Camera>().Render();
+             RenderTexture.active = rt;
+             screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+             GetComponent<Camera>().targetTexture = null;
+             RenderTexture.active = null; // JC: added to avoid errors
+             Destroy(rt);
+             byte[] bytes = screenShot.EncodeToPNG();
+             string filename = ScreenShotName(resWidth, resHeight);
+             System.IO.File.WriteAllBytes(filename, bytes);
+             Debug.Log(string.Format("Took screenshot to: {0}", filename));
+             takeHiResShot = false;
+         }
+     }
+ }
+
+    /* void LateUpdate() {
+         //takeHiResShot |= Input.GetKeyDown("k");
+        
+     }*/
+ }
+
+ 
---
+++ Assets/Scripts/SS1.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class SS1 : MonoBehaviour {
+    // Capture frames as a screenshot sequence. Images are
+    // stored as PNG files in a folder - these can be combined into
+    // a movie using image utility software (eg, QuickTime Pro).
+    // The folder to contain our screenshots.
+    // If the folder exists we will append numbers to create an empty folder.
+    string folder = "myscreenshots";
+    int frameRate = 15;
+        
+    void Start () {
+        // Set the playback framerate (real time will not relate to game time after this).
+        Time.captureFramerate = frameRate;
+        Debug.LogFormat((Time.captureFramerate).ToString());
+         System.IO.Directory.CreateDirectory(folder);
+         
+
+        StartCoroutine(CaptureScreen());
+ }
+        // Create the folder
+       
+            // Determine whether the directory exists.
+            
+public IEnumerator CaptureScreen()
+ {
+    // Wait till the last possible moment before screen rendering to hide the UI
+    yield return null;
+    GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
+ 
+    // Wait for screen rendering to complete
+    yield return new WaitForEndOfFrame();
+ 
+    // Take screenshot
+
+        string name = string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png", 
+                              Application.dataPath, 
+                              640, 480, 
+                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") );
+        
+        // Capture the screenshot to the specified file.
+        ScreenCapture.CaptureScreenshot(name);
+ 
+    // Show UI after we're done
+    GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+ }
+}
+    
+    
